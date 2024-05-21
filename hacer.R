ad1a1353d77e5bf0f52a4b6fda9accd0928bea4c
Diego J. Pedregal <diego.pedregal@uclm.es>
2024-05-21 10:37:24 +0200

    Add files via upload

---
+++ hacer.R
@@ -0,0 +1,30 @@
+
+
+** Canal de Bernard,y de Armesilla y de Vidal y de Rallo y de de Pablo el del Opus finanzas
+** Ejemplo de alquiler donde quiero por caro que sea, subempleo no trabajo de lo que quiero y no me pagan por hacer lo que yo quiero hacer... https://www.youtube.com/watch?v=CGnien_zYX4&ab_channel=JuanRam%C3%B3nRallo
+** Impuesto sobre la banca y elasticidad. Curva de Laffer y elasticidad...
+** Impuestos indirectos y descuento de gasolina de 20 céntimos.
+** https://youtu.be/kDsA_zRmFCo
+https://elpais.com/diario/1977/05/04/economia/231544820_850215.html
+
+https://es.catholic.net/op/articulos/45246/cat/423/la-doctrina-social-de-la-iglesia-frente-al-capital.html
+
+https://www.liberalismo.org/articulo/8/15/doctrina/social/iglesia/liberalismo/antagonismo/
+    
+    https://juandemariana.org/ijm-actualidad/analisis-diario/catolicismo-protestantismo-comunismo-capitalismo/
+    
+    https://ec.aciprensa.com/wiki/Liberalismo
+
+https://www.expansion.com/actualidadeconomica/analisis/2017/01/28/588c70e6e5fdea547c8b4645.html
+https://www.youtube.com/watch?v=2-VSj7bSsfo&list=RD2-VSj7bSsfo&start_radio=1&ab_channel=JussenVEVO
+
+
+RV: Intervencionismo precios Franco año 1956 y compararlo con el de hoy
+
+** Crear EXCEL para hacer ejercicios sencillos de contabilidad... para que cuadre activo con pasivo...
+
+
+
+
+
+
